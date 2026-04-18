7c2eed891e620b5791a9a61b1ed5e7c43dc1dc22
Allison Sy <sy.al@northeastern.edu>
2026-04-18 17:13:41 -0400

    added databse schema and mock data SQL files

---
+++ database-files/03_permission.sql
@@ -0,0 +1,15 @@
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (1, 'read', 'report');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (2, 'update', 'ticket');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (3, 'create', 'user');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (4, 'create', 'location');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (5, 'update', 'user');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (6, 'create', 'report');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (7, 'create', 'location');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (8, 'update', 'user');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (9, 'read', 'user');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (10, 'update', 'report');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (11, 'update', 'report');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (12, 'delete', 'ticket');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (13, 'delete', 'ticket');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (14, 'create', 'report');
+INSERT INTO Permission (permission_id, action_type, resource) VALUES (15, 'update', 'ticket');
---
+++ database-files/06_issue_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO Issue_Type (issue_type_id, issue_type_name, issue_category) VALUES (1, 'Entrance issue', 'Accessibility');
+INSERT INTO Issue_Type (issue_type_id, issue_type_name, issue_category) VALUES (2, 'Ramp issue', 'Accessibility');
+INSERT INTO Issue_Type (issue_type_id, issue_type_name, issue_category) VALUES (3, 'Sidewalk issue', 'Accessibility');
+INSERT INTO Issue_Type (issue_type_id, issue_type_name, issue_category) VALUES (4, 'Transport issue', 'Accessibility');
