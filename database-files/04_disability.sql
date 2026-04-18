368ec708ffa0883bf71bfade25e5bbd7b8f2e65a
Allison Sy <sy.al@northeastern.edu>
2026-04-18 17:13:41 -0400

    added databse schema and mock data SQL files

---
+++ database-files/04_disability.sql
@@ -0,0 +1,6 @@
+INSERT INTO Disability (disability_id, disability_name) VALUES (1, 'Visual');
+INSERT INTO Disability (disability_id, disability_name) VALUES (2, 'Visual');
+INSERT INTO Disability (disability_id, disability_name) VALUES (3, 'Mobility');
+INSERT INTO Disability (disability_id, disability_name) VALUES (4, 'Hearing');
+INSERT INTO Disability (disability_id, disability_name) VALUES (5, 'Mobility');
+INSERT INTO Disability (disability_id, disability_name) VALUES (6, 'None');
